c0e8d9e08d13910e27921d47fc05dd4898df12f7
nima-farnoodian <nima.farnoodian@outlook.com>
2022-08-12 13:45:41 +0200

    write function in car pooling overridden for supporting updating capacity. accept and refuse action in passenger model improved.

--- car_pooling/models/car_pooling.py
+++ car_pooling/models/car_pooling.py
@@ -15,7 +15,10 @@ class CarPooling(models.Model):
     destination_address=fields.Char(required=True)
     departure_date=fields.Date(copy=False,default=lambda self: fields.Datetime.now())
     departure_time = fields.Float('Departure Time',required=True)
-    capacity=fields.Integer(string="Available Seats",required=True)
+    capacity=fields.Integer(string="Number of seats",required=True)
+    filled_seat=fields.Integer(string="Number of filled seats",readonly=True)
+    available_seat=fields.Integer(compute="_compute_available_seat", store=True, string="available seats")
+
     status=fields.Selection(
         string="Status",
         selection=[("available","Available"),("full","Full"),("unavailable","Unavailable"),("departed","Departed"),('canceled','Canceled')],
@@ -27,9 +30,14 @@ class CarPooling(models.Model):
     return_time = fields.Float('Return Time',copy=False)
     # capacity_return=fields.Integer(string="Capacity for return", required=True)
     passenger_ids=fields.One2many("car.pooling.passenger","trip_id",string="Passengers")
-    
 
-   
+
+    @api.depends("capacity","filled_seat")
+    def _compute_available_seat(self):
+        for record in self:
+            record.available_seat = record.capacity- record.filled_seat
+
+ 
     def cancel_action(self):
         # This function is responsible for canceling a trip if the trip is not in "departed" status.
         for record in self:
@@ -48,14 +56,16 @@ class CarPooling(models.Model):
 
     _sql_constraints = [
         ('seat_no_check', 'CHECK(capacity >= 0)',
-         'The seat number cannot be negative!')
+         'The seat number cannot be negative!'),
+        ('available_seat_check', 'CHECK(filled_seat <= capacity)',
+         "The capacity of the vehicle must be equal to or greater than the number of filled seats! To reduce the capacity, refuse some passengers' accepted requests.")
     ]
 
     #On write and On-delete and create api should be added to avoid inconsistency (e.g., what if we update the capacity while it is in full status?)
     @api.model
     def create(self, vals):        
         if vals['capacity']==0:
-            raise UserError("The number of availabe seat should be greater than zero!")
+            raise UserError("The Number of seats (Vehicle Capacity) should be greater than zero!")
         # Then call super to execute the parent method
         return super(CarPooling,self).create(vals)
     @api.ondelete(at_uninstall=False)
@@ -64,12 +74,27 @@ class CarPooling(models.Model):
                 msg="There are some passengers in 'accepted' status for this trip. To delete the trip, please make sure you have refused all passenger book requests."   
                 print("Message:",msg)
                 raise UserError(msg)
-    # on_write for updating will be added here. (e.g., what if we update the capacity while it is in full status?)
+
+    def write (self,vals):
+        # on_write for updating capacity. (e.g., what if we update the capacity while it is in full status?)
+        if "filled_seat" in vals and "capacity" in vals:
+            if vals['capacity']- vals['filled_seat']==0 and self.status not in ('unavailable','departed','canceled'):
+                vals["status"]='full'
+            elif vals['capacity']- vals['filled_seat']>0  and self.status not in ('unavailable','departed','canceled'):
+                vals["status"]='available'
+        elif  "filled_seat" not in vals and "capacity" in vals:
+            if vals['capacity']- self.filled_seat==0 and self.status not in ('unavailable','departed','canceled'):
+                vals["status"]='full'
+            elif vals['capacity']- self.filled_seat>0  and self.status not in ('unavailable','departed','canceled'):
+                vals["status"]='available'
+        elif "filled_seat" in vals and "capacity" not in vals:
+            if self.capacity- vals['filled_seat']==0 and self.status not in ('unavailable','departed','canceled'):
+                vals["status"]='full'
+            elif self.capacity- vals['filled_seat']>0  and self.status not in ('unavailable','departed','canceled'):
+                vals["status"]='available'
+        super(CarPooling,self).write(vals)
     
     #TODO Book action with its button should be added such that it inserts the user to the passenger list
-
-
-    
 #############################################################
 class CarPoolingTag(models.Model):
     _name="car.pooling.tag"
@@ -100,14 +125,13 @@ class CarPoolingPassenger(models.Model):
         #TODO add automatic odoo message (Accpeted) sent to the passanger
         for record in self:
             if record.trip_id.status!="departed" and record.trip_id.status!="canceled":
-                record.status = "accepted"
-                if record.trip_id.capacity>0:
-                    record.trip_id.capacity=record.trip_id.capacity-1
+                if record.trip_id.filled_seat<record.trip_id.capacity:
+                    record.trip_id.filled_seat=record.trip_id.filled_seat+1
                     record.status="accepted"
-                    if  record.trip_id.capacity==0:
+                    if  record.trip_id.filled_seat==record.trip_id.capacity:
                         record.trip_id.status="full"
                 else:
-                    raise UserError("The vehicle does not have capacity for more passenger.")
+                    raise UserError("The vehicle does not have capacity for more passengers.")
             else:
                 raise UserError("No passenger can be added to a departed or canceled trip.")
         return True
@@ -117,7 +141,7 @@ class CarPoolingPassenger(models.Model):
         for record in self:
             if record.trip_id.status!="departed" and record.trip_id.status!="canceled":
                 record.status = "refused"
-                record.trip_id.capacity=record.trip_id.capacity+1
+                record.trip_id.filled_seat=record.trip_id.filled_seat-1
                 record.trip_id.status="available"
             else:
                 raise UserError("No passenger can be removed from a departed or canceled trip.")
